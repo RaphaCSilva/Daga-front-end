52f3e9785b1996d1f8d3c086e80249ae62329135
Raphael <raphaelcavalcantesilva1999@gmail.com>
2022-10-06 01:48:40 -0300

    feat: home page without css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
 
 export default function App() {
 
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="" element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   );
