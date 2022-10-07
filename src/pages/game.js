bbdf8f3e838c2e5add83bcd4d5d5c7bfdad29eb3
Raphael <raphaelcavalcantesilva1999@gmail.com>
2022-10-07 01:01:08 -0300

    feat: add coordinate system with css classes

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,11 +1,37 @@
 import React from "react";
+import styled from 'styled-components'
 
 export default function Game() {
 
-
+    const arr = [];
+    for(let i = 0; i <= 100; i++){
+        arr.push(i);
+    }
+    const abre = "{"
+    const string = "position: absolute;"
+    const fecha = "}"
     return (
         <>
+            <GameScreen>
+                <div className="X13 Y10">
 
+                </div>
+            </GameScreen>
         </>
     );
 }
+
+const GameScreen = styled.div`
+    width 600px;
+    height: 500px;
+    margin: auto;
+    margin-top: 10%;
+    background-color: red;
+    position: relative;
+
+    div{
+        width: 100px;
+        height: 100px;
+        background-color: blue;
+    }
+`;
